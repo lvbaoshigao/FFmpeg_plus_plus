a962c59255a0c6699951cc7eacc9be8a5fb4cbd9
Fox <69955056+434264329@users.noreply.github.com>
2026-09-06 17:25:43 +0800

    version update

--- server_cpp/src/dll_main.cpp
+++ server_cpp/src/dll_main.cpp
@@ -21,7 +21,7 @@
 using json = nlohmann::json;
 using namespace ffmpegpp;
 
-static const char* SERVER_VERSION = "5.2.0";
+static const char* SERVER_VERSION = "5.3.6";
 
 static std::thread g_workerThread;
 static std::atomic<bool> g_running{false};
